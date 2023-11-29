310a5dffa65b8c1180aa974286bd438985b28173
Victory <svistunova.vic@gmail.com>
2023-11-29 23:12:23 +0300

    модели

--- BAKE/catalog/models.py
+++ BAKE/catalog/models.py
@@ -9,6 +9,25 @@ class Product(models.Model):
     name = models.CharField(max_length=255, blank=True, default='', verbose_name=u"Название")
     price = models.PositiveIntegerField(blank=True, default=0)
     description = models.TextField(blank=True, default='')
+    photo = models.ImageField(upload_to="products")
+    weight = models.PositiveIntegerField(blank=True, default=0)
+    calories = models.PositiveIntegerField(blank=True, default=0)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, )
+    ingredients = models.ManyToManyField('Ingredient')
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    class Meta:
+        verbose_name = "Ингредиент"
+        verbose_name_plural = "Ингредиенты"
+
+    name = models.CharField(max_length=255, blank=True, default='', verbose_name=u"Название")
+
+    def __str__(self):
+        return self.name
 
 
 class Category(models.Model):
@@ -16,7 +35,8 @@ class Category(models.Model):
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
 
-    name = models.CharField(max_length=255, blank=True, default='', verbose_name=u"Название")
+    name = models.CharField(max_length=255, blank=True, default='', verbose_name=u"Название", db_index=True)
+    image = models.ImageField(upload_to="categories")
 
     def __str__(self):
         return self.name
--- BAKE/catalog/views.py
+++ BAKE/catalog/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
 
+from .models import *
+
+
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    if request.method == "GET":
+        category = request.GET['category']
+        products = Product.objects.all()
+        if category == '0':
+            category = Category(name='Все')
+        else:
+            category = Category.objects.get(id=category)
+            products = products.filter(category_id=category.id)
+        context = {
+            'products': products,
+            'categories': Category.objects.all(),
+            'selected_category': category
+        }
+
+    elif request.method == "POST":
+        pass
+
+    return render(request, 'index.html', context)
